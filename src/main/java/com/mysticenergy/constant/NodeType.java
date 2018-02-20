1e0d38162829bbd8690192c43673510af44822d5
Wasmir <leaf.wasmir@gmail.com>
2018-02-20 14:11:08 +0800

    add enum and threadpool

---
+++ src/main/java/com/mysticenergy/constant/NodeType.java
@@ -0,0 +1,14 @@
+package com.mysticenergy.constant;
+
+/**
+ * Name: NodeType
+ * Desc:
+ *
+ * @Author: wasmir
+ * @Date 2018/2/20
+ */
+public enum NodeType {
+    NORMAL,
+    LOGIC,
+    NOTIFY
+}
---
+++ src/main/java/com/mysticenergy/constant/NotifyType.java
@@ -0,0 +1,14 @@
+package com.mysticenergy.constant;
+
+/**
+ * Name: NotifyType
+ * Desc:
+ *
+ * @Author: wasmir
+ * @Date 2018/2/20
+ */
+public enum NotifyType {
+    TELEGRAM,
+    QQ,
+    WX
+}
